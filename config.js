e5f6c293fc51c779fb128a0965cbc298248dfdf5
chenyaa2580 <121753438+chenyaa2580@users.noreply.github.com>
2023-01-01 20:58:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "送给漂亮的彤Sir": "./imgs/xiaokeai.png",
+        "送给漂亮的彤Sir": "./imgs/shide.jpg",
         // "希望2022的迷茫与停滞": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
